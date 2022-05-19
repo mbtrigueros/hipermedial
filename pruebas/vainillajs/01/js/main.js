43d3b6d0959b9e3308be0101a0a258c04dd60dbd
mbtrigueros <belen@canvas.com>
2022-05-19 00:13:00 -0300

    Test 2: Trying a rectangle class

--- pruebas/vainillajs/01/js/main.js
+++ pruebas/vainillajs/01/js/main.js
@@ -73,6 +73,41 @@ class Circle {
   }
 }
 
+//Rectangle Class
+class Rectangle {
+  constructor(x, y, w, h, dx, dy, r, g, b, a) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.dx = dx;
+    this.dy = dy;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+
+  draw() {
+    //Making circles
+    context.fillRect(this.x, this.y, this.w, this.h);
+    context.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
+  }
+
+  move() {
+    //X + X Velocity (direction)
+    if (this.x > innerWidth || this.x < 0) {
+      this.dx = -this.dx;
+    }
+    this.x += this.dx;
+    //Y + Y Velocity (direction)
+    if (this.y > innerHeight || this.y < 0) {
+      this.dy = -this.dy;
+    }
+    this.y += this.dy;
+  }
+}
+
 let circles = [];
 
 for (let i = 0; i < 50; i++) {
@@ -88,12 +123,17 @@ for (let i = 0; i < 50; i++) {
   circles.push(new Circle(x, y, radius, dx, dy, r, g, b, a));
 }
 
+let rectangle = new Rectangle(100, 200, 200, 100, 5, 10, 200, 200, 0, 0.5);
+
 //Animation Loop
 function animate() {
   //With this function you make the loop
   requestAnimationFrame(animate);
   //context.clearRect(0, 0, innerWidth, innerHeight);
   //Making circles
+
+  rectangle.draw();
+  rectangle.move();
   circles.map((circle) => {
     circle.draw();
     circle.move();
