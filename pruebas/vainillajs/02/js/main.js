42557514f7b236ab06405b7e32270e4e0af25fcf
mbtrigueros <belen@canvas.com>
2022-05-30 01:39:09 -0300

    02: Starry Night City

--- pruebas/vainillajs/02/js/main.js
+++ pruebas/vainillajs/02/js/main.js
@@ -23,8 +23,8 @@ function random(min = 0, max = 100) {
 
 let windows = [];
 
-//Rectangle Class
-class Rectangle {
+//Building Class
+class Building {
   constructor(x, y, w, h){
     this.x = x;
     this.y = y;
@@ -39,20 +39,77 @@ class Rectangle {
   }
 
   window(offsetX, offsetY){
+    let x = this.x + offsetX;
+    let y = this.y + offsetY;
+    let w = 5;
+    let h = 5;
+    context.shadowBlur = 5;
+    context.shadowOffsetX = 0;
+    context.shadowOffsetY = 0;
+    context.shadowColor = 'white';
     context.fillStyle = 'white';
-    context.fillRect(this.x + offsetX, this.y + offsetY, 5, 5);
+    context.fillRect(x, y, w, h);
+  }
+
+  door(){
+    let x = this.x +this.w/4;
+    let y = canvas.height-20;
+    let w = this.w/2;
+    let h = 30;
+    context.shadowBlur = 5;
+    context.shadowColor = 'gray';
+    context.shadowColor = 'transparent';
+    context.fillStyle = 'white';
+    // context.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.color.a})`;
+    context.fillRect(x, y, w, h);
+    context.lineWidth = 1;
+    context.strokeStyle = 'white';
+    context.strokeRect(x, y, w, h);
   }
 
   draw(){
+    context.shadowBlur = 5;
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = 'gray';
     context.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.color.a})`;
     context.fillRect(this.x, this.y, this.w, this.h);
-    for (let x = 0; x < this.w-15; x+=10) {
-      for (let y = 0; y < this.h; y+=20) {
-        this.window(x+5, y+10);
+    context.lineWidth = 3;
+    context.strokeStyle = 'white';
+    context.strokeRect(this.x, this.y, this.w, this.h);
+    for (let x = 0; x < this.w/2; x+=10) {
+      for (let y = 0; y < this.h; y+=10) {
+        this.window(x+this.w/4, y+10);
       }
     }
+    // this.door();
   }
 }
+
+//Create Stars
+
+function stars(){
+  for (let index = 0; index < 1000; index++) {
+    let x = random(0, canvas.width);
+    let y = random(0, canvas.height);
+    let h = 1;
+    let w = 1;
+    let color = {
+      r: 255,
+      g: 255, 
+      b: 255, 
+      a: 1}
+    context.shadowBlur = 1;
+    context.shadowOffsetX = 1;
+    context.shadowOffsetY = 1;
+    context.shadowColor = 'white';
+    context.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+    context.fillRect(x, y, w, h);
+  }
+}
+
+stars();
+
 //JS Events
 //Reference: https://www.w3schools.com/js/js_events.asp
 
@@ -60,7 +117,7 @@ class Rectangle {
 const mouse = {
   x: undefined,
   y: undefined,
-};
+}
 
 canvas.addEventListener("click", (event) => {
   console.log(event); //The event argument is an object that has information about the event. Example: mouse position.
@@ -68,12 +125,13 @@ canvas.addEventListener("click", (event) => {
   mouse.x = event.offsetX;
   mouse.y = event.offsetY;
 
-  let rectangle = new Rectangle(mouse.x, mouse.y, random(50, 300)/2, mouse.y * canvas.height);
+  let x= mouse.x;
+  let y= mouse.y;
+  let w=random(100, 220)/2;
+  let h=(mouse.y * canvas.height);
 
-  rectangle.draw();
-
-
-  console.log(mouse);
-  console.log(rectangle);
+  let building = new Building(x,y, w, h);
+  building.draw();
 });
+
 
